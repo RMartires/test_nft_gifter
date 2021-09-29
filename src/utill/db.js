aa57e3d97c3b5c9e64f2f5e75a1d432328af8085
RMartires <rohitmartires14@gmail.com>
2021-09-29 09:37:35 +0530

    fix: added aynon auth

--- src/utill/db.js
+++ src/utill/db.js
@@ -1,11 +1,22 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth, signInAnonymously } from "firebase/auth";
 
 const firebaseApp = initializeApp({
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_PROJECT_ID
 });
+
+const auth = getAuth();
+signInAnonymously(auth).then(() => {
+    console.log("logged in");
+})
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ...
+    });
 
 const db = getFirestore();
 
