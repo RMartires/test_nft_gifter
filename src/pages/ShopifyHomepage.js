b2382e77a62f78df7b1aabb16dfcc30adf8c0edd
RMartires <rohitmartires14@gmail.com>
2022-03-01 14:57:09 +0530

    feat: auto redirect to /shopify

--- src/pages/ShopifyHomepage.js
+++ src/pages/ShopifyHomepage.js
@@ -1,10 +1,21 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import "./ShopifyHomepage.css"
 
 export default function ShopifyHomepage() {
     return (
-        <div>
-            <Navbar />
-        </div>
+        <iframe src="https://stoic-albattani-34d893.netlify.app/components/landingPages/SaaSProductLandingPage"
+            // title="W3Schools Free Online Web Tutorials"
+            // width="100%"
+            style={{
+                // position: 'absolute',
+                height: '100vh',
+                width: '100%',
+                border: 'none',
+                backgroundColor: 'white'
+            }}
+        >
+
+        </iframe >
     );
 }
