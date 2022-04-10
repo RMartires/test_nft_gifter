c1aa88455b64288caa2d333dba8f8c28cb1072af
RMartires <rohitmartires14@gmail.com>
2022-04-10 16:29:57 +0530

    langinpage-cssissue

--- src/pages/ShopifyHomepage.js
+++ src/pages/ShopifyHomepage.js
@@ -2,27 +2,16 @@ import React, { useEffect } from "react";
 import Navbar from "../components/Navbar";
 import ReactGA from 'react-ga';
 import "./ShopifyHomepage.css";
+import ShopifyLandingPage from "./ShopifyHomepage/demos/SaaSProductLandingPage";
 
 export default function ShopifyHomepage() {
 
-    useEffect(() => {
-        ReactGA.initialize('UA-148934470-2');
-        ReactGA.pageview(window.location.pathname);
-    }, []);
+    // useEffect(() => {
+    //     ReactGA.initialize('UA-148934470-2');
+    //     ReactGA.pageview(window.location.pathname);
+    // }, []);
 
     return (
-        <iframe src="https://stoic-albattani-34d893.netlify.app/components/landingPages/SaaSProductLandingPage"
-            // title="W3Schools Free Online Web Tutorials"
-            // width="100%"
-            style={{
-                // position: 'absolute',
-                height: '100vh',
-                width: '100%',
-                border: 'none',
-                backgroundColor: 'white'
-            }}
-        >
-
-        </iframe >
+        <ShopifyLandingPage />
     );
 }
